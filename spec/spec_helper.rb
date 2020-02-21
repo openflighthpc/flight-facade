d37a2808e35eda594e0218d68960bdc88da60272
William McCumsite <william.mccumstie@gmail.com>
2020-02-21 12:19:26 +0000

    Remove the included file and from the spec
    These include short cuts are no longer required as the code base
    is now correctly in modules

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,8 +31,6 @@ require 'vcr'
 
 require_relative 'fixtures/demo_cluster'
 
-include FlightFacade
-
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
